d18df22970ca34a3d9e6ba03bd9e7535b081dca5
ittzzzmmm <ittzzzmmm@gmail.com>
2026-02-07 17:58:54 +0700

    edit param name : trueStatement to bodyStatement

--- src/main/java/com/example/demo/ast/WhileStatement.java
+++ src/main/java/com/example/demo/ast/WhileStatement.java
@@ -2,11 +2,11 @@ package com.example.demo.ast;
 
 import com.example.demo.game.Context;
 
-public record WhileStatement(Expression condition, Statement trueStatement)implements Statement {
+public record WhileStatement(Expression condition, Statement bodyStatement)implements Statement {
     @Override
     public void eval(Context context){
         if(condition.eval(context.variables()) > 0){
-            trueStatement.eval(context);
+            bodyStatement.eval(context);
         }
     }
 }
