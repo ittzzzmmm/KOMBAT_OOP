eb2864ae6376aade238eead29c943cf548a0f3e9
ittzzzmmm <ittzzzmmm@gmail.com>
2026-02-09 20:44:33 +0700

    clean the format

--- src/main/java/com/example/demo/ast/IfStatement.java
+++ src/main/java/com/example/demo/ast/IfStatement.java
@@ -3,7 +3,6 @@ package com.example.demo.ast;
 import com.example.demo.game.Context;
 
 public record IfStatement(Expression condition, Statement thenStatement, Statement elseStatement) implements Statement{
-
     @Override
     public void eval(Context context){
         if(condition.eval(context) > 0){ // greater than 0
