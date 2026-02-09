327ab5613f3563e5fddbfd8b3de81458e8830ad2
ittzzzmmm <ittzzzmmm@gmail.com>
2026-02-09 17:04:36 +0700

    add scan() method here better

--- src/main/java/com/example/demo/game/Context.java
+++ src/main/java/com/example/demo/game/Context.java
@@ -1,10 +1,16 @@
 package com.example.demo.game;
 
+import com.example.demo.ast.Direction;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public record Context(GameState gameState,Minion minion,Map<String,Integer> variables) {
     public Context(GameState gameState,Minion minion){
             this(gameState,minion,new HashMap<>());
+    }
+
+    public int scan(String type, Direction direction){
+        return 0;
     }
 }
