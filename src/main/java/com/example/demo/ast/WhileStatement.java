528e112a5f4ebf01ed0ec3b44b13991796bf7ca7
ittzzzmmm <ittzzzmmm@gmail.com>
2026-02-09 21:56:36 +0700

    change MAX_LOOP to LIMIT_LOOP

--- src/main/java/com/example/demo/ast/WhileStatement.java
+++ src/main/java/com/example/demo/ast/WhileStatement.java
@@ -3,10 +3,10 @@ package com.example.demo.ast;
 import com.example.demo.game.Context;
 
 public record WhileStatement(Expression condition, Statement bodyStatement)implements Statement {
-    private static final int MAX_LOOP = 100;
+    private static final int LIMIT_LOOP = 100;
     @Override
     public void eval(Context context){
-        for(int i=0;condition.eval(context) > 0 && i < MAX_LOOP;i++){
+        for(int i=0;condition.eval(context) > 0 && i < LIMIT_LOOP;i++){
             bodyStatement.eval(context);
         }
     }
