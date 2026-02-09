1e4828c4230fabeea636b050fa11ffbfa0e31194
ittzzzmmm <ittzzzmmm@gmail.com>
2026-02-09 20:45:35 +0700

    correct eval()

--- src/main/java/com/example/demo/ast/WhileStatement.java
+++ src/main/java/com/example/demo/ast/WhileStatement.java
@@ -3,9 +3,10 @@ package com.example.demo.ast;
 import com.example.demo.game.Context;
 
 public record WhileStatement(Expression condition, Statement bodyStatement)implements Statement {
+    private static final int MAX_LOOP = 100;
     @Override
     public void eval(Context context){
-        if(condition.eval(context) > 0){
+        for(int i=0;condition.eval(context) > 0 && i < MAX_LOOP;i++){
             bodyStatement.eval(context);
         }
     }
