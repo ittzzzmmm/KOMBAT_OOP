4bd3960bb6967330ad6d7206b26b1df129dd82b4
ittzzzmmm <ittzzzmmm@gmail.com>
2026-02-09 16:56:12 +0700

    add atomicValue for scanning expression

---
+++ src/main/java/com/example/demo/ast/AllyExpression.java
@@ -0,0 +1,10 @@
+package com.example.demo.ast;
+
+import com.example.demo.game.Context;
+
+public class AllyExpression implements Expression{
+    @Override
+    public int eval(Context context){
+        return context.scan("ALLY",null);
+    }
+}
---
+++ src/main/java/com/example/demo/ast/NearbyExpression.java
@@ -0,0 +1,4 @@
+package com.example.demo.ast;
+
+public class NearbyExpression {
+}
