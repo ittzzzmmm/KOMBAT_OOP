d16cba3e73ae1fd82f3e7601bf86923d08c3eb75
ittzzzmmm <ittzzzmmm@gmail.com>
2026-02-10 16:23:23 +0700

    change file name

--- src/main/java/com/example/demo/ast/AssignmentCommand.java
+++ src/main/java/com/example/demo/ast/AssignmentCommand.java
@@ -2,7 +2,7 @@ package com.example.demo.ast;
 
 import com.example.demo.game.Context;
 
-public record AssignmentStatement(String variable, Expression expression) implements Command{
+public record AssignmentCommand(String variable, Expression expression) implements Command{
 
     @Override
     public void eval(Context context){
